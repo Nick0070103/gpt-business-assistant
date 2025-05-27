5c074fb5a0822903953bdbf935dfc4d5ed332a24
Nick0070103 <162137890+Nick0070103@users.noreply.github.com>
2025-05-27 16:34:33 +0300

    Update assistant_bot.py
    заменил сводка на svodka

--- assistant_bot.py
+++ assistant_bot.py
@@ -54,7 +54,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # --- Запуск ---
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("сводка", svodka))
+app.add_handler(CommandHandler("svodka", svodka))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("Бот запущен...")
